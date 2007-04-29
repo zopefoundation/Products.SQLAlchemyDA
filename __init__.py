7db9c712ce017958e580eec56ac2709814cb05b0
Andreas Jung <lists@zopyx.com>
2007-04-29 13:54:53 +0000

    added

---
+++ __init__.py
@@ -0,0 +1,14 @@
+##########################################################################
+# A DA-like integration of SQLAlchemy based on z3c.sqlalchemy
+#
+# (C) Zope Corporation and Contributors
+# Written by Andreas Jung for Haufe Mediengruppe, Freiburg, Germany
+# and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
+##########################################################################
+
+
+import z3c.sqlalchemy
+
+
+def initialize(context):
+    pass
