f9a16c2dfc95d0a67a5139f6dfe1ae5d33e1f22b
Andreas Jung <lists@zopyx.com>
2008-03-12 11:05:12 +0000

    added

---
+++ setup.py
@@ -0,0 +1,41 @@
+##########################################################################
+# SQLAlchemyDA
+# (C) 2008, ZOPYX Ltd & Co. KG, Tuebingen, Germany
+##########################################################################
+
+import os
+from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Programming Language :: Python',
+]
+
+version_file = os.path.join('Products', 'SQLAlchemyDA', 'version.txt')
+version = open(version_file).read().strip()
+
+readme_file= os.path.join('Products', 'SQLAlchemyDA', 'doc', 'README.txt')
+desc = open(readme_file).read().strip()
+
+long_description = desc 
+
+
+setup(name='Products.SQLAlchemyDA',
+      version=version,
+      license='ZPL (see LICENSE.txt)',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      maintainer='Andreas Jung',
+      maintainer_email='info@zopyx.com',
+      classifiers=CLASSIFIERS,
+      keywords='Zope2 Database adapter SQLAlchemy',
+      url='http://opensource.zopyx.com/projects/SQLAlchemyDA',
+      description='A generic database adapter for Zope 2',
+      long_description=long_description,
+      packages=['Products', 'Products.SQLAlchemyDA'],
+      include_package_data = True,
+      zip_safe=False,
+      install_requires=['setuptools', 'z3c.sqlalchemy'],
+      namespace_packages=['Products'],
+
+      )
