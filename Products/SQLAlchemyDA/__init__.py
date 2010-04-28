8d05b2216cd1d0b5b95ff523c179db1c2091d220
Abdul Kader <akm.mail@gmail.com>
2010-04-28 17:27:36 +0000

    SOFTWARE_HOME is no more available in Zope 2.12

--- Products/SQLAlchemyDA/__init__.py
+++ Products/SQLAlchemyDA/__init__.py
@@ -6,6 +6,7 @@
 # and ZOPYX Ltd. & Co. KG, Tuebingen, Germany
 ##########################################################################
 
+import os
 try:
     import z3c.sqlalchemy
 except ImportError:
@@ -14,12 +15,15 @@ except ImportError:
 from config import ADD_SA_WRAPPER_PERMISSION
 
 
-def initialize(context):
 
-    from da import (SAWrapper, manage_addSAWrapper, 
+def initialize(context):
+    from da import (SAWrapper, manage_addSAWrapper,
                    manage_addSAWrapperForm)
-    context.registerClass(SAWrapper, 
-                          constructors=(manage_addSAWrapperForm, 
+    from Shared.DC import ZRDB
+
+    icon_path = os.path.join(os.path.dirname(ZRDB.__file__), 'www', 'DBAdapterFolder_icon.gif')
+    context.registerClass(SAWrapper,
+                          constructors=(manage_addSAWrapperForm,
                                         manage_addSAWrapper),
-                          icon = SOFTWARE_HOME + '/Shared/DC/ZRDB/www/DBAdapterFolder_icon.gif',
-                          permission=ADD_SA_WRAPPER_PERMISSION)                          
+                          icon=icon_path,
+                          permission=ADD_SA_WRAPPER_PERMISSION)
