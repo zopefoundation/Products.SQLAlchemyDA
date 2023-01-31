cae31a86bcd645dfba74f6d3af71ca3c4b930329
Jens Vagelpohl <jens@plyp.com>
2023-01-31 14:48:58 +0100

    Drop support for Python 2.7, 3.5, 3.6.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(name='Products.SQLAlchemyDA',
       package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+      python_requires='>=3.7',
       install_requires=[
         'setuptools',
         'six',
