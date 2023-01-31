2c2df3bfe5bf1b817e764bef982ded24579d2dbe
Jens Vagelpohl <jens@plyp.com>
2023-01-31 14:47:24 +0100

    Bumped version for breaking release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Change log
 ==========
 
-1.1.1 (unreleased)
+2.0.0 (unreleased)
 ------------------
 
 
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.11',
 ]
 
-version = '1.1.1.dev0'
+version = '2.0.0.dev0'
 
 readme_file = 'README.rst'
 changes_file = 'CHANGES.rst'
